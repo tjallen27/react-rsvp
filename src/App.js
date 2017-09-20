8988ff735212fe4375ac70a2f43143351e5b921b
tjallen27 <tjallen91@hotmail.co.uk>
2017-09-20 21:08:33 +0100

    GuestName component added with isEditing toggle

--- src/App.js
+++ src/App.js
@@ -8,31 +8,40 @@ class App extends Component {
     guests: [
       {
         name: 'Treasure',
-        isConfirmed: false
+        isConfirmed: false,
+        isEditing: false
       },
       {
         name: 'Nic',
-        isConfirmed: true
+        isConfirmed: true,
+        isEditing: false
       },
       {
         name: 'Matt K',
-        isConfirmed: true
+        isConfirmed: true,
+        isEditing: true
       }
     ]
-  }
+  };
 
-  toggleConfirmationAt = indexToChange =>
+  toggleGuestPropertyAt = (property, indexToChange) =>
     this.setState({
       guests: this.state.guests.map((guest, index) => {
         if(index === indexToChange){
           return {
             ...guest,
-            isConfirmed: !guest.isConfirmed
+            [property]: !guest[property]
           };
         }
         return guest;
       })
     });
+
+  toggleConfirmationAt = index =>
+    this.toggleGuestPropertyAt("isConfirmed", index);
+
+  toggleEditingAt = index =>
+    this.toggleGuestPropertyAt("isEditing", index);
 
   getTotalInvited  = () => this.state.guests.length;
   // getAttendingGuests = () =>
@@ -75,7 +84,8 @@ class App extends Component {
 
           <GuestList
           guests={this.state.guests}
-          toggleConfirmationAt={this.toggleConfirmationAt} />
+          toggleConfirmationAt={this.toggleConfirmationAt}
+          toggleEditingAt = {this.toggleEditingAt} />
         </div>
       </div>
     );
